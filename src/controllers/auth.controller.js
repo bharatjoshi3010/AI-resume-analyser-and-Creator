2544aca80661840231cde95486b052b62647670c
unknown <bj20032003@gmail.com>
2026-06-27 09:31:19 +0530

    created register controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,64 @@
+const userModel = require("../models/user.model")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+/**
+ * @name registerUserController
+ * @description Register a new user, excepts username, email and password in body
+ * @access Public
+ */
+
+async function registerUserController(req, res) {
+
+    const { username, email, password } = req.body
+
+    if(!username || !email || !password) {
+        return res.status(400).json({
+            message: "Please provide userame, email and password"
+        })
+    }
+
+    const isUserAlreadyExists = await userModel.findOne({
+        $or: [ { username }, { email }]
+    })
+
+    if(isUserAlreadyExists) {
+        return res.status(400).json({
+            message: "Account already exists with this email address or username"
+        })
+    }
+
+    const hash = await bcrypt.hash(password, 10)
+
+    const user = await userModel.create({
+        username, 
+        email,
+        password: hash
+    })
+
+    const token = jwt.sign(
+        {
+            id: user._id,
+            username: user.username
+        },
+        process.env.JWT_SECRET,
+        { expiresIn : "1d" }
+    )
+
+    res.cookie("token", token)
+
+    //sending the status code 201(we send this when we make a new resource)
+    res.status(201).json({
+        message: "User registered successfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+
+}
+
+module.exports = {
+    registerUserController
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,16 @@
 const {Router} = require('express')   //we requiring router by destructuring it
+const authController = require("../controllers/auth.controller")
 
 const authRouter = Router()
 
+
+//these are js doc comments, it hepls to create documantation for the routes and functions (when you hover over this route anywhere in this code base you get all these info there)
+/**
+ * @route POST /api/auth/register
+ * @description  Register a new user
+ * @access Public
+ */
+
+authRouter.post("/register", authController.registerUserController)
 
 module.exports = authRouter
