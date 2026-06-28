cfc7025ec0c835add68f2ed85750918e3842076a
unknown <bj20032003@gmail.com>
2026-06-28 10:14:19 +0530

    created login form UI

---
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "../auth.form.scss"
+
+const Login = () => {
+
+
+    const handleSubmit = (e) => {          //prevent reload when we click on the submit button of form
+        e.preventDefault()
+    }
+
+  return (
+    <main>
+        <div className="form-container">
+            <h1>Login</h1>
+
+            <form>
+
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" placeholder='Enter email address' />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" placeholder='Enter your password' />
+                </div>
+
+                <button className='button primary-button'>Login</button>
+
+            </form>
+        </div>
+    </main>
+  )
+}
+
+export default Login
