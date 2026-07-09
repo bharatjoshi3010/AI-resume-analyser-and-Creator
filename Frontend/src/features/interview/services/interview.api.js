0452c07136ab3f86e454f56ba1d9084ff3a41b2c
unknown <bj20032003@gmail.com>
2026-07-09 17:59:05 +0530

    created get resume pdf hook

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -41,3 +41,14 @@ export const getAllInterviewReports = async() => {
 
     return response.data
 }
+
+/**
+ * @description Service to generate resume pdf based on user self description, resume content and job description
+ */
+export const generateResumePdf = async ({ interviewReportId }) => {
+    const response = await API.post(`/api/interview/resume/pdf/${interviewReportId}`, null, {
+        responseType: "blob"
+    })
+    
+    return response.data
+}
